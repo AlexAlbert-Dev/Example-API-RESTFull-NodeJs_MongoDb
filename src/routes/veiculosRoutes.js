b9901efd6c68fadb1306f9c4780f98ee437a52e0
Alex Albert Cosmo de Assis <108278362+AlexAlbert-Dev@users.noreply.github.com>
2022-08-16 11:24:48 -0300

    Criacao de rota para controller de veiculos

---
+++ src/routes/veiculosRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import VeiculoController from "../controllers/veiculosController";
+
+const router = express.Router();
+
+router
+    .get("/veiculos", VeiculoController.getAllVeiculos)
+
+export default router;
+
